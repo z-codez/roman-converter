13b04ca0a5ebe87758f24e7da1cfff70b6bc4d1d
dci-student <137389454+z-codez@users.noreply.github.com>
2025-02-04 12:00:44 +0100

    Logic 100% done

--- js/app.js
+++ js/app.js
@@ -2,17 +2,27 @@ const numInput = document.getElementById("num-input");
 const romanDisplay = document.getElementById("roman-display");
 const errorDisplay = document.getElementById("error-display");
 
-const form = document.querySelector("#converter");
+const btn = document.querySelector("button");
+const form = document.getElementById("converter");
 
 
 const arabNums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 const romanNums = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
 
+numInput.addEventListener("change", reset);
+
 form.addEventListener("submit", (e) => {
   // This prevents the form from reloading the page
   e.preventDefault();
-  getRomanChar(numInput.value);
+  const romanCharacters = getRomanChar(numInput.value);
+  displayRoman(romanCharacters);
 });
+
+
+
+function reset() {
+  romanDisplay.classList.add("hidden");
+}
 
 function getRomanChar() {
   let decimalValue = Number(numInput.value);
@@ -24,8 +34,12 @@ function getRomanChar() {
       decimalValue -= arabNums[i];
     }
   }
+  return romanNum;
+}
 
-  console.log(romanNum);
+function displayRoman(roman) {
+  romanDisplay.classList.remove("hidden");
+  romanDisplay.textContent = roman;
 }
 
 
